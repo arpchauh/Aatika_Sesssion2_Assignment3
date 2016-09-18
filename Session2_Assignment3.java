4909e1cf68e8d27e298092e209f381733bfcb45a
arpchauh <arpitchauhan10@gmail.com>
2016-09-18 14:56:58 +0530

    Add files via upload

---
+++ Session2_Assignment3.java
@@ -0,0 +1,33 @@
+package aatika;
+
+public class Session2_Assignment3 {
+
+	public static void main(String[] args) {
+		
+		int i,j,k;
+	
+		
+		for(i=1;i<=5;i++){
+			k=1;
+			
+			for(j=1;j<=i;j++){
+				
+				System.out.print(k);
+				k++;
+				}
+			System.out.println();
+			
+		}
+		for(i=4;i>=1;i--){
+			k=1;
+			for(j=1;j<=i;j++){
+				System.out.print(k);
+				k++;
+				
+			}System.out.println();
+			
+		}
+
+	}
+
+}
